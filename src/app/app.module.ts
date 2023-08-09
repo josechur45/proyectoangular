c7fd2ab7950814600a2efdea303cd3f047b859e4
Jose Maria Chura Mirabal <josechura45@gmail.com>
2023-08-08 22:44:54 -0300

    trabajo final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { IntroSectionComponent } from './intro-section/intro-section.component';
+import { AboutUsSectionComponent } from './about-us-section/about-us-section.component';
+import { ServicesSectionComponent } from './services-section/services-section.component';
+import { WhyUsSectionComponent } from './why-us-section/why-us-section.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ClientsSectionComponent } from './clients-section/clients-section.component';
+import { TeamSectionComponent } from './team-section/team-section.component';
+import { ContactSectionComponent } from './contact-section/contact-section.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    IntroSectionComponent,
+    AboutUsSectionComponent,
+    ServicesSectionComponent,
+    WhyUsSectionComponent,
+    PortfolioComponent,
+    ClientsSectionComponent,
+    TeamSectionComponent,
+    ContactSectionComponent,
+    TestimonialsComponent
   ],
   imports: [
     BrowserModule,
